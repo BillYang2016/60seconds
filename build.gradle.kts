a7bcc1fc8f8c2da3779fcada70c172cf627abc78
Bill_Yang <ym13883407586@outlook.com>
2021-10-06 10:30:24 +0800

    modified

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,10 @@ plugins {
     val kotlinVersion = "1.5.10"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
-    id("net.mamoe.mirai-console") version "2.7.0"
+    id("net.mamoe.mirai-console") version "2.7.1"
 }
 
-group = "org.example"
+group = "com.billyang"
 version = "0.1.0"
 
 repositories {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,5 +7,4 @@ pluginManagement {
         maven("https://dl.bintray.com/kotlin/kotlin-eap")
     }
 }
-//这里改生成的插件名称
 rootProject.name = "mirai-example"
